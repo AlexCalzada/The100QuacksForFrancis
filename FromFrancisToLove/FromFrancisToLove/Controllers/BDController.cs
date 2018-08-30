94b861ce3f91025cae9751428d5d5d211df7d1e5
Alex CG <alex.clz.glz95@gmail.com>
2018-08-30 15:20:16 -0500

    p

---
+++ FromFrancisToLove/FromFrancisToLove/Controllers/BDController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using FromFrancisToLove.Data;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FromFrancisToLove.Controllers
+{
+    [Route("PruebaBD")]
+    public class BDController : Controller
+    {
+        private readonly HouseOfCards_Context _context;
+
+        public BDController(HouseOfCards_Context context)
+        {
+            
+            _context = context;
+            //if (_context.Producto.Count()==0)
+            //{
+                
+            //}
+
+        }
+
+  
+       // [HttpGet("0")]
+        //public List<> GetBD()
+        //{
+        //    return _context.Producto.ToList();
+        //}
+
+        // GET api/values/5
+    
+    }
+}
