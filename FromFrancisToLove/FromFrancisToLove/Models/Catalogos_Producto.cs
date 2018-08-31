9de3eeca323ae3d4467ebbc6538a41104305497c
Rene perales rodriguez <Rene11DS@hotmail.com>
2018-08-31 12:13:33 -0500

    NuevasTablas y Llaves Foraneas  de las Tablas Conexion_Config Tienda_Config Catalogo_Producto

---
+++ FromFrancisToLove/FromFrancisToLove/Models/Catalogos_Producto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FromFrancisToLove.Models
+{
+    [Table("CATALOGO_PRODUCTO", Schema = "HouseOfCards")]
+    public class Catalogos_Producto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string SKU { get; set; }
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+
+        public int ConfigId { get; set; }
+
+        [ForeignKey("ConfigId")]
+        public virtual Conexion_Config User { get; set; }
+    }
+}
