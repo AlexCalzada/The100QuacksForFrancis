c74da5f7b8ed1386987df2fc3fe9057af4176d0f
Rene perales rodriguez <Rene11DS@hotmail.com>
2018-08-31 11:29:55 -0500

    Modelos

--- FromFrancisToLove/FromFrancisToLove/Models/Movimientos.cs
+++ FromFrancisToLove/FromFrancisToLove/Models/Movimientos.cs
@@ -7,43 +7,33 @@ using System.Threading.Tasks;
 
 namespace FromFrancisToLove.Models
 {
-    [Table("PRODUCTO", Schema = "HouseOfCards")]
-    public class Productos
+    [Table("MOVIMIENTO", Schema = "HouseOfCards")]
+    public class Movimientos
     {
-  /*FECHA_LOCAL DATE,
-  HORA_LOCAL TIME,
-  FECHA_CONTABLE DATE,
-  TRANSACCION INT(11),
-  SKU VARCHAR(13),
-  TIPO_PAGO VARCHAR(20),
-  REFERENCIA_1 VARCHAR(120),
-  REFERENCIA_2 VARCHAR(120),
-  MONTO DECIMAL(10,2),
-  DV INT(4),
-  COMISION DECIMAL(10,2),
-  TOKEN VARCHAR(8),
-  AUTORIZACION CHAR(20),
-  SERIE VARCHAR(19)        
-         */
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int MovimientoId { get; set; }
-        public int GrupoId { get; set; }
+
+        public int MovimientoID { get; set; }
         public int CadenaId { get; set; }
-        public int TiendaId { get; set; }
+
+        public int  TiendaId { get; set; }
         public int PosId { get; set; }
         public int CajeroId { get; set; }
-        public string Fecha_Local { get; set; }
-        public string Hora_Local { get; set; }
-        public string Fecha_Contable { get; set; }
+        public DateTime Fecha_Local { get; set; }
+        public DateTime Hora_Local { get; set; }
+        public DateTime Fecha_Contable { get; set; }
         public int Transaccion { get; set; }
+        public string SKU { get; set; }
         public string Referencia_1 { get; set; }
         public string Referencia_2 { get; set; }
         public decimal Monto { get; set; }
         public int DV { get; set; }
         public decimal Comision { get; set; }
-
         public string Token { get; set; }
+        public string Autorizacion { get; set; }
+
+        public string Serie { get; set; }
+
 
 
     }
