2dcd8877dcc1b2b7a4870aac0babf10897d49a78
Alex CG <alex.clz.glz95@gmail.com>
2018-09-05 16:41:47 -0500

    QueryRequest Funcionando

--- FromFrancisToLove/FromFrancisToLove/Controllers/CatalogoProductosController.cs
+++ FromFrancisToLove/FromFrancisToLove/Controllers/CatalogoProductosController.cs
@@ -71,32 +71,51 @@ namespace FromFrancisToLove.Controllers
         {
             try
             {
-                var RelReq = new ReloadData();
-                RelReq.ID_GRP = 7;
-                RelReq.ID_CHAIN = 1;
-                RelReq.ID_MERCHANT = 1;
-                RelReq.ID_POS = 1;
-                RelReq.DateTime = DateTime.Now.ToString();
-                //RelReq.SKU = "8469760101006";
-                RelReq.PhoneNumber = "8661625268";
-                RelReq.TransNumber = 1020;
-                RelReq.ID_Product = "SBH001";
-                //RelReq.ID_COUNTRY = 484;
-                //RelReq.TC = 0;
-                RelReq.Brand = "TELCEL";
-                RelReq.Instr1 = "MARCA *264";
-                RelReq.Instr2 = "VIGENCIA TIEMPO AIRE 30 DIAS";
-                RelReq.AutoNo = 0;
-                RelReq.ResponseCode = 00;
-                RelReq.DescripcionCode = "Reload Success";
-                RelReq.Monto = 1521;
+                //var RelReq = new ReloadData();
+                //RelReq.ID_GRP = 7;
+                //RelReq.ID_CHAIN = 1;
+                //RelReq.ID_MERCHANT = 1;
+                //RelReq.ID_POS = 1;
+                //RelReq.DateTime = DateTime.Now.ToString();
+                ////RelReq.SKU = "7378840101007";
+                //RelReq.PhoneNumber = "1020304050";
+                //RelReq.TransNumber = 1020;
+                //RelReq.ID_Product = "SBH001";
+                ////RelReq.ID_COUNTRY = 0484;
+                ////RelReq.TC = 0;
+                //RelReq.Brand = "TELCEL";
+                //RelReq.Instr1 = "MARCA *264";
+                //RelReq.Instr2 = "VIGENCIA TIEMPO AIRE 23 DIAS";
+                //RelReq.AutoNo = 0;
+                //RelReq.ResponseCode = 00;
+                //RelReq.DescripcionCode = "Reload Success";
+                //RelReq.Monto = 1340;
 
+                var queryReq = new QueryRequest();
+                queryReq.ID_GRP = 7;
+                queryReq.ID_CHAIN = 1;
+                queryReq.ID_MERCHANT = 1;
+                queryReq.ID_POS = 1;
+                queryReq.DateTime = DateTime.Now.ToString();
+                queryReq.SKU = "7378840101007";
+                queryReq.PhoneNumber = "8661425585";
+                queryReq.TransNumber = 1020;
+                //RelReq.ID_Product = "SBH001";
+                queryReq.ID_COUNTRY = 0484;
+                queryReq.TC = 0;
+                //RelReq.Brand = "TELCEL";
+                //RelReq.Instr1 = "MARCA *264";
+                //RelReq.Instr2 = "VIGENCIA TIEMPO AIRE 23 DIAS";
+                //RelReq.AutoNo = 0;
+                //RelReq.ResponseCode = 00;
+                //RelReq.DescripcionCode = "Reload Success";
+                //RelReq.Monto = 1340;
 
-                XmlSerializer xmlSerializer = new XmlSerializer(RelReq.GetType());
+                XmlSerializer xmlSerializer = new XmlSerializer(queryReq.GetType());
 
                 StringWriter sw = new StringWriter();
                 XmlWriter writer = XmlWriter.Create(sw);
-                xmlSerializer.Serialize(writer, RelReq);
+                xmlSerializer.Serialize(writer, queryReq);
                 var xml = sw.ToString();
 
                 
@@ -109,7 +128,7 @@ namespace FromFrancisToLove.Controllers
 
                 //var x = xml.ToString();
 
-                var recarga = client.getReloadDataAsync(xml.ToString());
+                var recarga = client.getQueryClassAsync(xml.ToString());
 
                 return Ok(recarga.Result);
             }
---
+++ FromFrancisToLove/FromFrancisToLove/Requests/QueryRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FromFrancisToLove.Requests
+{
+    public class QueryRequest
+    {
+        public int ID_GRP { get; set; }
+        public int ID_CHAIN { get; set; }
+        public int ID_MERCHANT { get; set; }
+        public int ID_POS { get; set; }
+        public string DateTime { get; set; }
+        public string SKU { get; set; }
+        public string PhoneNumber { get; set; }
+        public int TransNumber { get; set; }
+        public int ID_COUNTRY { get; set; }
+        public int TC { get; set; }
+    }
+}
