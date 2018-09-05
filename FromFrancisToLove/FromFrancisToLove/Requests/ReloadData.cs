9b3c0fd39d8f39b8123dee8f843c7937b6e31274
Rene perales rodriguez <Rene11DS@hotmail.com>
2018-09-05 16:33:12 -0500

    c

---
+++ FromFrancisToLove/FromFrancisToLove/Requests/ReloadData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace FromFrancisToLove.Requests
+{
+    public class ReloadData
+    {
+        public int ID_GRP { get; set; }
+        public int ID_CHAIN { get; set; }
+        public int ID_MERCHANT { get; set; }
+        public int ID_POS { get; set; }
+        public string DateTime { get; set; }
+        //public string SKU { get; set; }
+        public string PhoneNumber { get; set; }
+        public int TransNumber { get; set; }
+        public string ID_Product { get; set; }
+        //public int ID_COUNTRY { get; set; }
+        //public int TC { get; set; }
+        public string Brand { get; set; }
+        public string Instr1 { get; set; }
+        public string Instr2 { get; set; }
+        public int AutoNo { get; set; }
+        public int ResponseCode { get; set; }
+        public string DescripcionCode { get; set; }
+        public int Monto { get; set; }
+    }
+}
