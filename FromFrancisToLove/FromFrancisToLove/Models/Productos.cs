a643d3dc70b07fa5d3427caa1e7bddacd0e1678d
Alex CG <alex.clz.glz95@gmail.com>
2018-09-12 12:45:33 -0500

    o

--- FromFrancisToLove/FromFrancisToLove/Models/Productos.cs
+++ FromFrancisToLove/FromFrancisToLove/Models/Productos.cs
@@ -10,21 +10,6 @@ namespace FromFrancisToLove.Models
     [Table("PRODUCTO", Schema = "HouseOfCards")]
     public class Productos
     {
-  /*FECHA_LOCAL DATE,
-  HORA_LOCAL TIME,
-  FECHA_CONTABLE DATE,
-  TRANSACCION INT(11),
-  SKU VARCHAR(13),
-  TIPO_PAGO VARCHAR(20),
-  REFERENCIA_1 VARCHAR(120),
-  REFERENCIA_2 VARCHAR(120),
-  MONTO DECIMAL(10,2),
-  DV INT(4),
-  COMISION DECIMAL(10,2),
-  TOKEN VARCHAR(8),
-  AUTORIZACION CHAR(20),
-  SERIE VARCHAR(19)        
-         */
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int MovimientoId { get; set; }
---
+++ FromFrancisToLove/FromFrancisToLove/Requests/ServiceBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FromFrancisToLove.Requests
+{
+    public class ServiceBase
+    {
+        public int ID_GRP { get; set; }
+        public int ID_CHAIN { get; set; }
+        public int ID_MERCHANT { get; set; }
+        public int ID_POS { get; set; }
+        public string DateTime { get; set; }
+        public string SKU { get; set; }
+        public string PhoneNumber { get; set; }
+        public int TransNumber { get; set; }
+        public string ID_Product { get; set; }
+        public int ID_COUNTRY { get; set; }
+        public int TC { get; set; }
+        public string Brand { get; set; }
+        public string Instr1 { get; set; }
+        public string Instr2 { get; set; }
+        public int AutoNo { get; set; }
+        public int ResponseCode { get; set; }
+        public string DescripcionCode { get; set; }
+        public int Monto { get; set; }
+    }
+
+    public class ReloadRequest : ServiceBase { }
+    public class ReloadResponse : ServiceBase { }
+    public class QueryRequest : ServiceBase { }
+    public class QueryResponse : ServiceBase { }
+    public class ReloadRequestData : ServiceBase { }
+    public class ReloadResponseData : ServiceBase { }
+    public class QueryRequestData : ServiceBase { }
+    public class QueryResponseData : ServiceBase { }
+}
